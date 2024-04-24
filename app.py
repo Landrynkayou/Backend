0d89be3c46f2e9233b1608be43e5b124dc50424e
SAMUEL ATEM <152394299+Samuelatem@users.noreply.github.com>
2024-04-24 19:19:06 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask
+
+from views.candidate import candidate_view
+from views.voters import voters_view
+from views.session import session_view
+
+UPLOAD_FOLDER = os.path.join(
+    os.path.dirname(__file__), 
+    'uploads'
+)
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app.register_blueprint(candidate_view)
+app.register_blueprint(voters_view)
+app.register_blueprint(session_view)
